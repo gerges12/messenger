3b4e290d90f77da467e48c2aa74a51efdc2fd3a4
gsaid <gsaid@LPCAIQ0782.ejadacairo.local>
2022-09-21 19:22:38 +0200

    add controller for block

---
+++ messanger/src/main/java/com/example/messanger/controller/BlockcCntroller.java
@@ -0,0 +1,12 @@
+package com.example.messanger.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("conversation")
+public class BlockcCntroller {
+	
+	
+
+}
---
+++ messanger/src/main/java/com/example/messanger/service/BlockService.java
@@ -0,0 +1,8 @@
+package com.example.messanger.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlockService {
+
+}
