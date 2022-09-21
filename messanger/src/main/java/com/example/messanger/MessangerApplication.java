220d731ed79b305bd918fa41dd5df97d860a0539
gsaid <gsaid@LPCAIQ0782.ejadacairo.local>
2022-09-21 16:08:54 +0200

    add comment

--- messanger/src/main/java/com/example/messanger/MessangerApplication.java
+++ messanger/src/main/java/com/example/messanger/MessangerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessangerApplication {
 
 	public static void main(String[] args) {
+		
+		//
 		SpringApplication.run(MessangerApplication.class, args);
 	}
 
